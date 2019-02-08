371bbd829ac076ea0460acc394506267706f8d0a
jm.kagaya@gmail.com <jm.kagaya@gmail.com>
2019-02-09 00:22:27 +0900

    done
    done

--- Project1/Project1/Form1.Designer.vb
+++ Project1/Project1/Form1.Designer.vb
@@ -22,9 +22,50 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("MS UI Gothic", 50.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label1.Location = New System.Drawing.Point(122, 131)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(97, 67)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "10"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(87, 281)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(175, 105)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(370, 478)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Stop_Watch"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Timer1 As Timer
 End Class
--- Project1/Project1/Form1.vb
+++ Project1/Project1/Form1.vb
@@ -1,3 +1,30 @@
 ﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
+        If Timer1.Enabled Then
+            Timer1.Enabled = False
+            Button1.Text = "Start"
+
+        Else
+
+            Timer1.Enabled = True
+            Button1.Text = "Stop"
+
+        End If
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        Label1.Text = Label1.Text - 1
+
+        If Label1.Text = 0 Then
+            Timer1.Enabled = False
+            Button1.Text = "Reset"
+            MsgBox("Time out")
+            Button1.Text = "Start"
+            Label1.Text = "10"
+        End If
+
+    End Sub
 End Class
